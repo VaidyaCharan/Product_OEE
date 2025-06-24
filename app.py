433bcaaf9a1fb30b44470677766c37c5c997e528
Charan Vaidya <charan.n.vaidya@gmail.com>
2025-06-24 06:49:43 +0530

    Changes Done by Charan Vaidya

--- app.py
+++ app.py
@@ -231,4 +231,4 @@ def export_excel(machine_id):
 
 # ========== Run Server ==========
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=8000)
+    serve(app, host='0.0.0.0', port=5000)
